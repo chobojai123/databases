92c4a7dd56c3aa7112e058fa842b61502e53e749
chobojai123 <davidcheng1290@gmail.com>
2018-03-30 20:41:38 -0700

    working on models

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,35 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var dbConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'student',
+  password: 'student',
+  database: 'chat'
+  
+});
 
+dbConnection.connect(function(err) {
+  if (err) {
+    throw err;
+  } 
+  console.log('Connected!');
+});
+
+exports.dbQuery = function() {
+  dbConnection.query('SELECT * FROM messages', function (err, result, field) {
+    if (err) {
+      throw err;
+    }
+    console.log(result);
+  });
+};
+
+exports.dbPost = function(user, timeCreated) {
+  dbConnection.query(`INSERT INTO messages (text, createdAt) VALUES (${user}, ${timeCreated})`, function (err, result, field) {
+    if (err) {
+      throw err;
+    }
+    console.log(result);
+  });
+};
--- server/schema.sql
+++ server/schema.sql
@@ -1,14 +1,30 @@
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+
+
+CREATE TABLE users (
+  id integer AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+  name VARCHAR(20) NOT NULL
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  id integer AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+  name VARCHAR(20) NOT NULL
+);
 
-
+CREATE TABLE messages (
+  id integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  message VARCHAR(150) NOT NULL, 
+  createdAt VARCHAR(30) NOT NULL,
+  rooms_id integer,
+  users_id integer,
+  -- INDEX r_id (rooms_id),
+  CONSTRAINT FOREIGN KEY(rooms_id) references rooms(id),
+  CONSTRAINT FOREIGN KEY(users_id) references users(id)
+);
 
 
 /*  Execute this file from the command line by typing:
